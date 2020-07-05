bd1a6d7ef98a950eee0a86d2d559847cb9d2edca
xxzou <z1624440939@outlook.com>
2020-07-05 15:52:47 +0800

    nothing

--- Task2_Sybyl_align/autoclick.py
+++ Task2_Sybyl_align/autoclick.py
@@ -1,3 +1,18 @@
 '''
 pip install pywin32 -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install Pillow -i https://pypi.tuna.tsinghua.edu.cn/simple
 '''
+
+from PIL import ImageGrab
+import win32api,win32con
+import time
+time0 = time.time()
+
+def LeftClick(x,y):
+    win32api.SetCursorPos([x,y])
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+
+time.sleep(3)
+while time.time() < time0 + 20:
+    LeftClick(610,550)
+    time.sleep(0.3)
